73d912ef7e8e61ab0ecfa20703709915a9657b77
yonghwan <yonghwankim.dev@gmail.com>
2022-03-07 16:17:18 +0900

    code convenstion

---
+++ other/code_conventions/Statements.java
@@ -0,0 +1,27 @@
+package other.code_conventions;
+
+public class Statements {
+	public Statements() {
+		/* 각 줄에는 최대 하나의 명령문으로 구성되어야 함*/
+		int a = 1;
+		a++;	// 옮은 방법
+		a--;	// 옳은 방법
+		a++; a--;	// 옳지 않은 방법
+	}
+	
+	// return 구문은 반환 값을 명확하게 만들지 않는 한 괄호 사용은 지양함
+	public void returnMethod1() {
+		// return someMethod1(arg1, arg2)	권장하지 않음
+		return;
+	}
+	
+	public int returnMethod2() {
+		int[] arr = new int[4];
+		return arr.length;
+	}
+	
+	public int returnMethod3() {
+		int[] arr = new int[4];
+		return (arr.length == 4 ? arr.length : 4);	// 명확하므로 괜찮음
+	}
+}
