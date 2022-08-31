34f0cc8e90fbc95c345d561b9c91b9c3a970b4c1
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-08-31 14:57:33 +0900

    [추가] PriorityQueue

---
+++ ch11/ex04_queue/PriorityQueueTest.java
@@ -0,0 +1,29 @@
+package ch11.ex04_queue;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class PriorityQueueTest {
+    @Test
+    public void offer_poll() throws Exception{
+        //given
+        Queue<Integer> pq = new PriorityQueue<>();
+        List<Integer> actual = new ArrayList<>();
+        //when
+        pq.offer(1);
+        pq.offer(5);
+        pq.offer(2);
+        pq.offer(4);
+        pq.offer(3);
+
+        while(!pq.isEmpty()){
+            actual.add(pq.poll());
+        }
+        //then
+        assertThat(actual).isEqualTo(Arrays.asList(1,2,3,4,5));
+    }
+}
